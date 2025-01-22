0c0e4bff3d2fc86b712a6ff1636e3bab3b658559
Mauro <maurolobo.ml@gmail.com>
2025-01-22 09:57:18 -0300

    agrego los errores que marcaron

--- src/app/[lang]/components/home.js
+++ src/app/[lang]/components/home.js
@@ -28,7 +28,7 @@ export function VideoBackground() {
   return (
     <div className="relative">
       <video
-        className="fixed top-0 left-0 w-screen h-screen object-cover z-[-10]"
+        className="fixed md:absolute top-0 left-0 w-screen h-screen object-cover z-[-10]"
         autoPlay
         loop
         muted
